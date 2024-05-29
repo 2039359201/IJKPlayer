7d313abaf4221f9495d70801cc05e5bc26d5aaaa
2039359201 <2733113918@xiaomi.com>
2024-05-29 12:33:37 +0800

    增加音频流代码 TODO: 音视频同步

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -25,11 +25,11 @@ extern "C" {
 }
 
 
-AVFormatContext * avFormatContext;
+AVFormatContext *avFormatContext;
 AVCodecContext *videoContext;
 AVCodecContext *audioContext;
-int videoIndex =-1;
-int audioIndex =-1;
+int videoIndex = -1;
+int audioIndex = -1;
 //视频队列和音频队列
 MNQueue *videoQueue;
 MNQueue *audioQueue;
@@ -46,48 +46,50 @@ int videoHeight;
 
 //swsContext
 SwsContext *swsContext;
+//音频转换上下文
 SwrContext *swrContext;
 //nativeWindow
 ANativeWindow *nativeWindow;
 //nativeWindow_Buffer
 ANativeWindow_Buffer nativeWindow_buffer;
 
-//全局thiz
+//全局变量
 jobject mthiz;
 
 //回调方法
 jmethodID playTrack;
-_JavaVM *javaVM=NULL;
+_JavaVM *javaVM = NULL;
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
     jint result = -1;
     javaVM = vm;
     JNIEnv *env;
-    if (vm->GetEnv((void **) &env, JNI_VERSION_1_6) != JNI_OK) {
+    if (vm->GetEnv((void **) &env, JNI_VERSION_1_4) != JNI_OK) {
         return result;
     }
-    return JNI_VERSION_1_6;
+    return JNI_VERSION_1_4;
 }
 
 //读取线程方法
 bool isStart = false;
-void *decodePacket(void *pVoid){
+
+void *decodePacket(void *pVoid) {
     LOGI("==========解码线程");
-    while(isStart){
-        if(audioQueue->size()>50||videoQueue->size()>50){
+    while (isStart) {
+        if (audioQueue->size() > 50 || videoQueue->size() > 50) {
             continue;
         }
         //创建avpacket 空容器
-        AVPacket *avPacket=av_packet_alloc();
-        if(av_read_frame(avFormatContext,avPacket)<0){
+        AVPacket *avPacket = av_packet_alloc();
+        if (av_read_frame(avFormatContext, avPacket) < 0) {
             break;
         }
 
-        if(avPacket->stream_index==videoIndex){
+        if (avPacket->stream_index == videoIndex) {
             //视频流方法
             videoQueue->push(avPacket);
 
 
-        }else if(avPacket->stream_index==audioIndex){
+        } else if (avPacket->stream_index == audioIndex) {
             //音频流方法
             audioQueue->push(avPacket);
         }
@@ -98,38 +100,39 @@ void *decodePacket(void *pVoid){
 }
 
 //视频解码线程
-void *decodeVideo(void *pVoid){
+void *decodeVideo(void *pVoid) {
     LOGI("==========视频解码线程");
-    while(isStart){
-        AVPacket *videoPacket=av_packet_alloc();
+    while (isStart) {
+        AVPacket *videoPacket = av_packet_alloc();
         videoQueue->getAvPacket(videoPacket);
         LOGI("视频解码");
         //解码
-        int ret=avcodec_send_packet(videoContext,videoPacket);
+        int ret = avcodec_send_packet(videoContext, videoPacket);
         //挂载数据流
-        AVFrame *videoFrame=av_frame_alloc();
-        ret=avcodec_receive_frame(videoContext,videoFrame);
+        AVFrame *videoFrame = av_frame_alloc();
+        ret = avcodec_receive_frame(videoContext, videoFrame);
 
         LOGI("解码成功");
         //rgb转换yuva
-        sws_scale(swsContext,videoFrame->data,videoFrame->linesize,0,videoHeight,rgbFrame->data,rgbFrame->linesize);
+        sws_scale(swsContext, videoFrame->data, videoFrame->linesize, 0, videoHeight,
+                  rgbFrame->data, rgbFrame->linesize);
 
         //绘制
-        ANativeWindow_lock(nativeWindow,&nativeWindow_buffer,NULL);
-        uint8_t *dst= (uint8_t *) nativeWindow_buffer.bits;
-        int destStride=nativeWindow_buffer.stride*4;
-        uint8_t *src=rgbFrame->data[0];
-        int srcStride=rgbFrame->linesize[0];
-        for(int i=0;i<videoHeight;i++){
-            memcpy(dst+i*destStride,src+i*srcStride,srcStride);
+        ANativeWindow_lock(nativeWindow, &nativeWindow_buffer, NULL);
+        uint8_t * dst = (uint8_t *) nativeWindow_buffer.bits;
+        int destStride = nativeWindow_buffer.stride * 4;
+        uint8_t * src = rgbFrame->data[0];
+        int srcStride = rgbFrame->linesize[0];
+        for (int i = 0; i < videoHeight; i++) {
+            memcpy(dst + i * destStride, src + i * srcStride, srcStride);
         }
         ANativeWindow_unlockAndPost(nativeWindow);
         av_frame_free(&videoFrame);
         av_free(videoFrame);
-        videoFrame=NULL;
+        videoFrame = NULL;
         av_packet_free(&videoPacket);
         av_free(videoPacket);
-        videoPacket=NULL;
+        videoPacket = NULL;
 
     }
     return NULL;
@@ -138,55 +141,70 @@ void *decodeVideo(void *pVoid){
 //音频解码线程
 void *decodeAudio(void *pVoid) {
     LOGI("==========音频解码线程");
-    AVFrame *audioFrame=av_frame_alloc();
-    swrContext=swr_alloc();
+    AVFrame *audioFrame = av_frame_alloc();
+    swrContext = swr_alloc();
     //设置参数
-    uint64_t out_ch_layout=AV_CH_LAYOUT_STEREO;
-    enum AVSampleFormat out_format=AV_SAMPLE_FMT_S16;
-    int out_sample_rate=audioContext->sample_rate;
-    swr_alloc_set_opts(swrContext,out_ch_layout,out_format,out_sample_rate,
-                       audioContext->channel_layout,audioContext->sample_fmt,audioContext->sample_rate,0,NULL);
+    uint64_t out_ch_layout = AV_CH_LAYOUT_STEREO;
+    enum AVSampleFormat out_format = AV_SAMPLE_FMT_S16;
+    int out_sample_rate = audioContext->sample_rate;
+    swr_alloc_set_opts(swrContext, out_ch_layout, out_format, out_sample_rate,
+                       audioContext->channel_layout, audioContext->sample_fmt,
+                       audioContext->sample_rate, 0, NULL);
     swr_init(swrContext);
 
-    int out_channels_nb=av_get_channel_layout_nb_channels(AV_CH_LAYOUT_STEREO);
+    int out_channels_nb = av_get_channel_layout_nb_channels(AV_CH_LAYOUT_STEREO);
 
-    uint8_t *out_buffer=(uint8_t *)av_malloc(44100*2);
-    while(isStart) {
+    uint8_t * out_buffer = (uint8_t *) av_malloc(44100 * 2);
+    while (isStart) {
         AVPacket *audioPacket = av_packet_alloc();
         audioQueue->getAvPacket(audioPacket);
         LOGI("音频 数据包 大小 %d", audioPacket->size);
         //解码
         int ret = avcodec_send_packet(audioContext, audioPacket);
+        if(ret<0 && ret!=AVERROR(EAGAIN) && ret!=AVERROR_EOF){
+            LOGI("音频解码失败 %d",ret);
+            continue;
+        }
         //挂载数据流
-
         ret = avcodec_receive_frame(audioContext, audioFrame);
 
-//        if(ret>=0){
-//            swr_convert(swrContext,&out_buffer,44100*2,(const uint8_t **)audioFrame->data,audioFrame->nb_samples);
-//            int size= av_samples_get_buffer_size(NULL,out_channels_nb,audioFrame->nb_samples,out_format,1);
-//        //回调java方法
-//         JNIEnv *jniEnv;
-//         LOGD("获取JNIEnv");
-//         if(javaVM->AttachCurrentThread(&jniEnv,NULL)!=JNI_OK){
-//             continue;
-//         }
-//         jbyteArray byteArrays =jniEnv->NewByteArray(size);
-//         jniEnv->SetByteArrayRegion(byteArrays,0,size,(const jbyte *)out_buffer);
-//         jniEnv->CallVoidMethod(mthiz,playTrack,byteArrays,size);
-//         jniEnv->DeleteLocalRef(byteArrays);
-//            javaVM->DetachCurrentThread();
-//    }
-           LOGI("解码后 大小:%d",audioFrame->pkt_size);
+        if(ret!=0){
+            av_packet_free(&audioPacket);
+            av_free(audioPacket);
+            audioPacket = NULL;
+            continue;
         }
+        if(ret>=0) {
+            swr_convert(swrContext, &out_buffer, 44100 * 2, (const uint8_t **) (audioFrame->data),
+                        audioFrame->nb_samples);
+            //解码大小
+            int size = av_samples_get_buffer_size(NULL, out_channels_nb, audioFrame->nb_samples,
+                                                  out_format, 1);
+            //回调java方法
+            JNIEnv *jniEnv;
+            LOGD("获取pcm数据 %d  ", size);
+            if (javaVM->AttachCurrentThread(&jniEnv, 0) != JNI_OK) {
+                continue;
+            }
+            //字节数组的转换 c->java
+            jbyteArray byteArrays = jniEnv->NewByteArray(size);
+            jniEnv->SetByteArrayRegion(byteArrays, 0, size, (const jbyte *) out_buffer);
+            jniEnv->CallVoidMethod(mthiz, playTrack, byteArrays, size);
+            jniEnv->DeleteLocalRef(byteArrays);
+            javaVM->DetachCurrentThread();
 
-        LOGI("解码成功");
+        }
+        LOGI("解码后 大小:%d", audioFrame->pkt_size);
+    }
 
-        //重采样
+    LOGI("解码成功");
+
+    //重采样
 
 
     av_frame_free(&audioFrame);
     av_free(audioFrame);
-    audioFrame=NULL;
+    audioFrame = NULL;
     avcodec_close(audioContext);
     avformat_close_input(&avFormatContext);
     return NULL;
@@ -198,25 +216,25 @@ extern "C"
 JNIEXPORT void JNICALL
 Java_com_example_ijkplayer_MNPlayer_play(JNIEnv *env, jobject thiz, jstring url_, jobject surface) {
     // TODO: implement play()
-    const char *url = env->GetStringUTFChars(url_,0);//在 env->ReleaseStringUTFChars(url_,url);处释放
-    LOGI("url:%s",url);
+    const char *url = env->GetStringUTFChars(url_, 0);//在 env->ReleaseStringUTFChars(url_,url);处释放
+    LOGI("url:%s", url);
     //直播流
     avformat_network_init();
-    avFormatContext=avformat_alloc_context();
+    avFormatContext = avformat_alloc_context();
     //打开视频
     if (avformat_open_input(&avFormatContext, url, NULL, NULL) < 0) {
         LOGI("无法打开文件: %s", url);
-    }else{
+    } else {
         //验证视频是否可用
         if (avformat_find_stream_info(avFormatContext, NULL) < 0) {
             LOGI("无法获取流信息");
             avformat_close_input(&avFormatContext);
         }
     }
-    LOGI("流数目:%d",avFormatContext->nb_streams);
+    LOGI("流数目:%d", avFormatContext->nb_streams);
 
     //视频流
-    for(int i=0;i<avFormatContext->nb_streams;i++) {
+    for (int i = 0; i < avFormatContext->nb_streams; i++) {
         if (AVMEDIA_TYPE_VIDEO == avFormatContext->streams[i]->codecpar->codec_type) {//视频流
             videoIndex = i;
             LOGI("视频%d", i);
@@ -248,51 +266,55 @@ Java_com_example_ijkplayer_MNPlayer_play(JNIEnv *env, jobject thiz, jstring url_
             avcodec_open2(audioContext, dec, 0);
         }
     }
-        //获取宽高
-        videoWidth=videoContext->width;
-        videoHeight=videoContext->height;
+    //获取宽高
+    videoWidth = videoContext->width;
+    videoHeight = videoContext->height;
 
-        //回调jmethodID
-        jmethodID onSizeChanged=env->GetMethodID(env->GetObjectClass(thiz),"onSizeChanged","(II)V");
-        env->CallVoidMethod(thiz,onSizeChanged,videoWidth,videoHeight);
+    //回调jmethodID
+    jmethodID onSizeChanged = env->GetMethodID(env->GetObjectClass(thiz), "onSizeChanged", "(II)V");
+    env->CallVoidMethod(thiz, onSizeChanged, videoWidth, videoHeight);
 
-        //回调jmethodID
-//        jmethodID createAudio=env->GetMethodID(env->GetObjectClass(thiz),"createTrack","(II)V");
-//        env->CallVoidMethod(thiz,createAudio,videoContext->sample_rate,audioContext->channels);
+    //回调jmethodID
+    jmethodID createAudio=env->GetMethodID(env->GetObjectClass(thiz),"createTrack","(II)V");
+    env->CallVoidMethod(thiz,createAudio,44100,av_get_channel_layout_nb_channels(AV_CH_LAYOUT_STEREO));
 //
-//        mthiz=env->NewGlobalRef(thiz);
-//        playTrack = env->GetMethodID(env->GetObjectClass(thiz),"playTrack","([BI)V");
+    //回调jmethodID
+    mthiz = env->NewGlobalRef(thiz);
+    playTrack = env->GetMethodID(env->GetObjectClass(mthiz),"playTrack","([BI)V");
 
 
-        rgbFrame = av_frame_alloc();
-        //设置rgbFrame的参数
-        int numBytes =av_image_get_buffer_size(AV_PIX_FMT_RGBA,videoWidth,videoHeight,1);
-        //初始化out_buffer
-        out_buffer=(uint8_t *)av_malloc(numBytes*sizeof(uint8_t));
+    rgbFrame = av_frame_alloc();
+    //设置rgbFrame的参数
+    int numBytes = av_image_get_buffer_size(AV_PIX_FMT_RGBA, videoWidth, videoHeight, 1);
+    //初始化out_buffer
+    out_buffer = (uint8_t *) av_malloc(numBytes * sizeof(uint8_t));
 
-        //将rgbFrame的数据设置到out_buffer
-        av_image_fill_arrays(rgbFrame->data,rgbFrame->linesize,out_buffer,AV_PIX_FMT_RGBA,videoWidth,videoHeight,1);
+    //将rgbFrame的数据设置到out_buffer
+    av_image_fill_arrays(rgbFrame->data, rgbFrame->linesize, out_buffer, AV_PIX_FMT_RGBA,
+                         videoWidth, videoHeight, 1);
 
-        //初始化swsContext
-        swsContext=sws_getContext(videoWidth,videoHeight,videoContext->pix_fmt,videoWidth,videoHeight,AV_PIX_FMT_RGBA,SWS_BICUBIC,NULL,NULL,NULL);
+    //初始化swsContext
+    swsContext = sws_getContext(videoWidth, videoHeight, videoContext->pix_fmt, videoWidth,
+                                videoHeight, AV_PIX_FMT_RGBA, SWS_BICUBIC, NULL, NULL, NULL);
 
-        nativeWindow=ANativeWindow_fromSurface(env,surface);
-        ANativeWindow_setBuffersGeometry(nativeWindow,videoWidth,videoHeight,WINDOW_FORMAT_RGBA_8888);
-        //初始化队列
-        audioQueue=new MNQueue;
-        videoQueue=new MNQueue;
-        //开辟线程
-        isStart=true;
-        pthread_t thread_decode;
-        pthread_t thread_video;
-        pthread_t thread_audio;
+    nativeWindow = ANativeWindow_fromSurface(env, surface);
+    ANativeWindow_setBuffersGeometry(nativeWindow, videoWidth, videoHeight,
+                                     WINDOW_FORMAT_RGBA_8888);
+    //初始化队列
+    audioQueue = new MNQueue;
+    videoQueue = new MNQueue;
+    //开辟线程
+    isStart = true;
+    pthread_t thread_decode;
+    pthread_t thread_video;
+    pthread_t thread_audio;
 
-        //读取线程
-        pthread_create(&thread_decode,NULL, decodePacket,NULL);
-        //视频解码线程
-        pthread_create(&thread_video,NULL, decodeVideo,NULL);
-        //音频解码线程
-        pthread_create(&thread_audio,NULL, decodeAudio,NULL);
+    //读取线程
+    pthread_create(&thread_decode, NULL, decodePacket, NULL);
+    //视频解码线程
+    pthread_create(&thread_video, NULL, decodeVideo, NULL);
+    //音频解码线程
+    pthread_create(&thread_audio, NULL, decodeAudio, NULL);
 
-    env->ReleaseStringUTFChars(url_,url);
+    env->ReleaseStringUTFChars(url_, url);
 }
--- app/src/main/java/com/example/ijkplayer/MNPlayer.java
+++ app/src/main/java/com/example/ijkplayer/MNPlayer.java
@@ -2,7 +2,9 @@ package com.example.ijkplayer;
 
 import android.media.AudioAttributes;
 import android.media.AudioFormat;
+import android.media.AudioManager;
 import android.media.AudioTrack;
+import android.util.Log;
 import android.view.Surface;
 import android.view.SurfaceView;
 import android.widget.RelativeLayout;
@@ -11,56 +13,65 @@ public class MNPlayer {
     static {
         System.loadLibrary("ijkplayer");
     }
+
     SurfaceView surfaceView;
 
     //Audio Track
     private AudioTrack audioTrack;
-    public MNPlayer(SurfaceView surfaceView){
-        this.surfaceView=surfaceView;
+
+    public MNPlayer(SurfaceView surfaceView) {
+        this.surfaceView = surfaceView;
     }
 
-    public native void play(String url,Surface surface);
+    public native void play(String url, Surface surface);
 
     //通道数 采样位数 频率
-//    public void createTrack(int sampleRateInHz,int nb_channals){
-//        int channelConfig;
-//        if(nb_channals==1){
-//            channelConfig = android.media.AudioFormat.CHANNEL_OUT_MONO;
-//        }else if(nb_channals==2){
-//            channelConfig = android.media.AudioFormat.CHANNEL_OUT_STEREO;
-//        }else{
-//            channelConfig = android.media.AudioFormat.CHANNEL_OUT_MONO;
-//        }
-//        int minBufferSize = android.media.AudioTrack.getMinBufferSize(sampleRateInHz,channelConfig,android.media.AudioFormat.ENCODING_PCM_16BIT);
-//        // 使用 AudioTrack.Builder 创建 AudioTrack 实例
-//        AudioTrack audioTrack = new AudioTrack.Builder()
-//                .setAudioAttributes(new AudioAttributes.Builder()
-//                        .setUsage(AudioAttributes.USAGE_MEDIA)
-//                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-//                        .build())
-//                .setAudioFormat(new AudioFormat.Builder()
-//                        .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
-//                        .setSampleRate(sampleRateInHz)
-//                        .setChannelMask(channelConfig)
-//                        .build())
-//                .setBufferSizeInBytes(minBufferSize)
-//                .build();
-//
-//        audioTrack.play();
-//
-//    }
-//    public void playTrack(byte[] audioData,int length){
-//        audioTrack.write(audioData,0,length);
-//    }
+    public void createTrack(int sampleRateInHz, int nb_channals) {
+        Log.d("audioTrack","sampleRateInHz:"+sampleRateInHz+" nb_channals:"+nb_channals);
+        int channelConfig;
+        if (nb_channals == 1) {
+            channelConfig = android.media.AudioFormat.CHANNEL_OUT_MONO;
+        } else if (nb_channals == 2) {
+            channelConfig = android.media.AudioFormat.CHANNEL_OUT_STEREO;
+        } else {
+            channelConfig = android.media.AudioFormat.CHANNEL_OUT_MONO;
+        }
+        int minBufferSize = android.media.AudioTrack.getMinBufferSize(sampleRateInHz, channelConfig, android.media.AudioFormat.ENCODING_PCM_16BIT);
+        // 使用 AudioTrack.Builder 创建 AudioTrack 实例
+        audioTrack = new AudioTrack.Builder()
+                .setAudioAttributes(new AudioAttributes.Builder()
+                        .setUsage(AudioAttributes.USAGE_MEDIA)
+                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                        .build())
+                .setAudioFormat(new AudioFormat.Builder()
+                        .setSampleRate(sampleRateInHz)
+                        .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                        .setChannelMask(channelConfig)
+                        .build())
+                .setBufferSizeInBytes(minBufferSize)
+                .setTransferMode(AudioTrack.MODE_STREAM)
+                .build();
 
-    public void onSizeChanged(int width,int height){
-        float ratio = (float)width/height;
+        audioTrack.play();
+
+        //释放
+        //audioTrack.release();
+
+
+    }
+    public void playTrack(byte[] audioData,int length){
+        Log.i("audioTrack","playTrack"+audioData.length);
+        audioTrack.write(audioData,0,length);
+    }
+
+    public void onSizeChanged(int width, int height) {
+        float ratio = (float) width / height;
         int screenwidth = surfaceView.getContext().getResources().getDisplayMetrics().widthPixels;
-        int videoWidth =0;
-        int videoHeight =0;
+        int videoWidth = 0;
+        int videoHeight = 0;
         videoWidth = screenwidth;
-        videoHeight = (int) (screenwidth/ratio);
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(videoWidth,videoHeight);
+        videoHeight = (int) (screenwidth / ratio);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(videoWidth, videoHeight);
         //调整播放器大小
         surfaceView.setLayoutParams(params);
     }
